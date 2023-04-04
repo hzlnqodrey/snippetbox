e2df546fef8f116c5bd0c7c65deeca285e4dad93
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-04 12:05:38 +0700

    basic https mux golang - introduction chapter 2.3 - Web Application Basic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hzlnqodrey/snippetbox.git
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello form Snippetbox"))
+}
+
+func main() {
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servemux we just created. If http.ListenAndServe() returns an er
+	// we use the log.Fatal() function to log the error message and exit.
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
