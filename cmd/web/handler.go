9012d47b9ee1030cef32be74590b600adc25df39
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-17 10:09:05 +0700

    Chap 2.7 Proj Structure and Refactoring Code Base

---
+++ cmd/web/handler.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	
+	if err != nil  || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Displaying a specific snippet with ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "Post")
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet"))
+}
