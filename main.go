2aad4c063df7adfe74b88eb83d76ae8b0a67f6c6
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-17 09:07:42 +0700

    Chap 2.6 - URL Query Settings

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -18,7 +20,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Displaying a specific snippet........."))
+	// Chapter 2.6 - URL QUERY SETTINGS
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snipped with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
