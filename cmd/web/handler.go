81798d741fd2a12cec747ec253956c612a5ed4d9
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-17 10:42:14 +0700

    Chap 2.8 UI Template

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,13 +14,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	
-	if err != nil  || id < 1 {
+
+	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
