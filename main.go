4e5cf1c952d873427818136f93b7c0410e6e5437
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-04-17 08:53:30 +0700

    suppressing system-generated headers

--- main.go
+++ main.go
@@ -28,15 +28,21 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		// the second parameter is the header value.
 		w.Header().Set("Allow", "POST")
 
-		// Manipulating the header map
+		// == Manipulating the header map
+		// Set a new cache-control header. If an existing "Cache-Control" header exists
+		// it will be overwritten.
 		w.Header().Set("Cache-Control", "public, max-age=31536000")
-
+		// In contrast, the Add() method appends a new "Cache-Control" header and can
+		// be called multiple times.
 		w.Header().Add("Cache-Control", "public")
 		w.Header().Add("Cache-Control", "max-age=31536000")
-
+		// Delete all values for the "Cache-Control" header.
 		w.Header().Del("Cache-Control")
-
+		// Retrieve the first value for the "Cache-Control" header.
 		w.Header().Get("Cache-Control")
+
+		// == Supressing system-generated header
+		w.Header()["Date"] = nil
 
 		// 	w.WriteHeader(405)
 		// 	w.Write([]byte("Method not allowed"))
