cfa515ac3f5863fb7b82be6bcce0155d22a185f1
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-07-06 01:45:22 +0700

    Chapter 4.4 - Designing Database Models, create Snippet Controller in snippets.go

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -1,1 +1,27 @@
-test
+package mysql
+
+import (
+	"database/sql"
+
+	"github.com/hzlnqodrey/snippetbox.git/pkg/models"
+)
+
+// Define a SnippetModel type which wraps a sql.DB connection pool.
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// POST SNIPPETS
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	return 0, nil
+}
+
+// GET SNIPPETS
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+// GET 10 RECENT SNIPPETS
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
