6f363f816fce7cf47661391bd65906f3cc230739
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-07-09 16:37:53 +0700

    Chapter 4.6 - Single Record SQL Queries

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -18,7 +18,7 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
 	result, err := m.DB.Exec(stmt, title, content, expires)
-	
+
 	if err != nil {
 		return 0, nil
 	}
@@ -34,7 +34,27 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
 // GET SNIPPETS
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+
+	// Chap 4.6 - Single-record SQL Queries
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// Chap 4.6 - Single-record SQL Queries
+	row := m.DB.QueryRow(stmt, id)
+
+	// Chap 4.6 - Single-record SQL Queries
+	// Initialize a pointer to a new zeroed Snippet struct
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	// everything works, return the snippet object
+	return s, nil
 }
 
 // GET 10 RECENT SNIPPETS
